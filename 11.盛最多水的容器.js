ef98db1640dbea42be8007246f63a8e3e12c1f13
dhlin <542072149@qq.com>
2020-06-18 22:56:35 +0800

    双指针移动-关键在于何时移动指针

--- 11.盛最多水的容器.js
+++ 11.盛最多水的容器.js
@@ -15,12 +15,24 @@ var maxArea = function(height) {
 
     let max = 0
 
-    for (let i = 0; i < len - 1; i ++) {
-        for (let j = i + 1; j < len; j ++) {
-            const h1 = height[i]
-            const h2 = height[j]
-            const m = Math.min(h2, h1) * (j - i)
-            max = max > m ? max : m
+    // for (let i = 0; i < len - 1; i ++) {
+    //     for (let j = i + 1; j < len; j ++) {
+    //         const h1 = height[i]
+    //         const h2 = height[j]
+    //         const m = Math.min(h2, h1) * (j - i)
+    //         max = max > m ? max : m
+    //     }
+    // }
+
+    // 双指针的关键在于如何移动指针
+    for (let i = 0, j = len - 1; i < j;) {
+        const h1 = height[i]
+        const h2 = height[j]
+        max = Math.max(max, Math.min(h2, h1) * (j - i))
+        if (h1 < h2) {
+            i ++
+        } else {
+            j --
         }
     }
 
