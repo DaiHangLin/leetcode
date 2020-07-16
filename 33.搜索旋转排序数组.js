f3403604a73ad7c5617ed8800e50b29c4d8560d2
dhlin <542072149@qq.com>
2020-07-16 08:28:15 +0800

    双指针，如何移动指针找到目标值

--- 33.搜索旋转排序数组.js
+++ 33.搜索旋转排序数组.js
@@ -11,7 +11,35 @@
  * @return {number}
  */
 var search = function(nums, target) {
-
+    if (!nums || nums.length <= 0) {
+        return -1
+    }
+    const len = nums.length
+    let left = 0;
+    let right = len - 1
+    while (left <= right) {
+        const first = nums[left]
+        const last = nums[right]
+    
+        if (target < first && target > last) {
+            return -1
+        }
+        if (target > first) {
+            left ++
+            continue
+        }
+        if (target === first) {
+            return left
+        }
+        if (target < last) {
+            right --
+            continue
+        }
+        if (target === last) {
+            return right
+        }
+    }
+    return -1
 };
 // @lc code=end
 
