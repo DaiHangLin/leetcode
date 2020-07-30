6184391fa7d92ea29998c58dd48d709e880781f8
dhlin <542072149@qq.com>
2020-07-30 08:13:25 +0800

    不懂啊

--- 53.最大子序和.js
+++ 53.最大子序和.js
@@ -9,30 +9,38 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-    let len = nums.length
+// var maxSubArray = function(nums) {
+//     let len = nums.length
 
-    function max(l, r) {
-        if (l == r) {
-            return nums[l]
-        }
-        let mid = (l + r) >> 1
-        let maxLeft = max(l, mid)
-        let maxRight = max(mid + 1,  r)
-        let leftMaxnum = 0,rightMaxnum = 0;
-        let sum=0;
-        for(let i=mid-1;i>=l;i--){
-            sum+=nums[i];
-            leftMaxnum=Math.max(leftMaxnum,sum);
-        }
-        sum=0;
-        for(let j=mid+1;j<=r;j++){
-            sum+=nums[j];
-            rightMaxnum=Math.max(rightMaxnum,sum);
-        }
-        return Math.max(leftMaxnum + nums[mid] + rightMaxnum, Math.max(maxLeft, maxRight))
-    }
-    return max(0, len - 1)
+//     function max(l, r) {
+//         if (l == r) {
+//             return nums[l]
+//         }
+//         let mid = (l + r) >> 1
+//         let maxLeft = max(l, mid)
+//         let maxRight = max(mid + 1,  r)
+//         let leftMaxnum = 0,rightMaxnum = 0;
+//         let sum=0;
+//         for(let i=mid-1;i>=l;i--){
+//             sum+=nums[i];
+//             leftMaxnum=Math.max(leftMaxnum,sum);
+//         }
+//         sum=0;
+//         for(let j=mid+1;j<=r;j++){
+//             sum+=nums[j];
+//             rightMaxnum=Math.max(rightMaxnum,sum);
+//         }
+//         return Math.max(leftMaxnum + nums[mid] + rightMaxnum, Math.max(maxLeft, maxRight))
+//     }
+//     return max(0, len - 1)
+// };
+var maxSubArray = function(nums) {
+    let pre = 0, maxAns = nums[0];
+    nums.forEach((x) => {
+        pre = Math.max(pre + x, x);
+        maxAns = Math.max(maxAns, pre);
+    });
+    return maxAns;
 };
 
 // @lc code=end
