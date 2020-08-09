06be97c1d6959984e3369bf68352ff3cbf7b8db8
dhlin <542072149@qq.com>
2020-08-10 07:34:27 +0800

    旋转矩阵

--- 48.旋转图像.js
+++ 48.旋转图像.js
@@ -10,7 +10,24 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
+     let n = matrix.length;
 
+    // transpose matrix
+    for (let i = 0; i < n; i++) {
+      for (let j = i + 1; j < n; j++) {
+        let tmp = matrix[j][i];
+        matrix[j][i] = matrix[i][j];
+        matrix[i][j] = tmp;
+      }
+    }
+    // reverse each row
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n / 2; j++) {
+        let tmp = matrix[i][j];
+        matrix[i][j] = matrix[i][n - j - 1];
+        matrix[i][n - j - 1] = tmp;
+      }
+    }
 };
 // @lc code=end
 
