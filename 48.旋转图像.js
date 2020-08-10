3d7d23ce809a4a9bdf7a451c01c6be92b24f5696
dhlin <542072149@qq.com>
2020-08-10 08:09:44 +0800

    上下翻转，对角线翻转

--- 48.旋转图像.js
+++ 48.旋转图像.js
@@ -10,24 +10,21 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-     let n = matrix.length;
-
-    // transpose matrix
-    for (let i = 0; i < n; i++) {
-      for (let j = i + 1; j < n; j++) {
-        let tmp = matrix[j][i];
-        matrix[j][i] = matrix[i][j];
-        matrix[i][j] = tmp;
-      }
-    }
-    // reverse each row
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < n / 2; j++) {
-        let tmp = matrix[i][j];
-        matrix[i][j] = matrix[i][n - j - 1];
-        matrix[i][n - j - 1] = tmp;
-      }
-    }
+     let n = matrix.length;        
+        //上下翻转        
+        for (let i = 0; i < n / 2; i ++){            
+            let tmp = matrix[i];            
+            matrix[i] = matrix[n - i - 1];            
+            matrix[n - i - 1] = tmp;       
+        }        
+        //对角线翻转        
+        for (let i = 0; i < n; i ++){            
+            for (let j= i + 1; j < n; j++){                
+                let tmp = matrix[i][j];                
+                matrix[i][j] = matrix[j][i];                
+                matrix[j][i] = tmp;            
+            }       
+        } 
 };
 // @lc code=end
 
